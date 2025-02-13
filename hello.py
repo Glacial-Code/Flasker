b832d5a0a23825704243a4046c1470cf66f1cd88
Glacial <GlacialDubs@proton.me>
2025-02-13 11:11:45 +0800

    added database, userform, unfinished.

--- hello.py
+++ hello.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 app.config['SECRET_KEY'] = 'my secret key'
+db = SQLAlchemy(app)
+
+# create db model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create A String
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+class UserForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 
 class NamerForm(FlaskForm):
@@ -12,8 +33,21 @@ class NamerForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    form = UserForm()
+    name = None
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+    return render_template("add_user.html", form=form)
 
+
+@app.route('/')
 #def index():
 #   return "<h1>Hello World</h1>"
 def index():
